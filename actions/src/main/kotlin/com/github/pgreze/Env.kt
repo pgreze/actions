cbe357d40773b502eea3290f2867e067164f6629
pgreze <pierrick.greze@gmail.com>
2022-08-11 20:01:58 +0200

    Update kotlin

--- actions/src/main/kotlin/com/github/pgreze/Env.kt
+++ actions/src/main/kotlin/com/github/pgreze/Env.kt
@@ -1,7 +1,9 @@
 package com.github.pgreze
 
 fun requireEnv(name: String): String =
-    System.getenv(name) ?: throw NullPointerException("Missing environment variable $name")
+    System.getenv(name)
+        ?: throw NullPointerException("Missing environment variable $name")
 
 fun requireNonBlankEnv(name: String): String =
-    requireEnv(name).takeIf(String::isNotBlank) ?: throw NullPointerException("Invalid environment variable $name")
+    requireEnv(name).takeIf(String::isNotBlank)
+        ?: throw NullPointerException("Invalid environment variable $name")
